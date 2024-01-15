eb52a68925571e9feeefb036c4f9fc8d886bb2a5
Your Name <ahmedabuayoub2@gmail.com>
2024-01-15 14:25:10 -0800

    error4

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,21 +4,22 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s -singly linked list
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * fot Holborton project
+ * for Holberton project
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
-size_t print_listint(const listin_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listin(listint_t *head);
-int chheck_cycle(listint_t *list);
 
-#endof /* LISTS_H */
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
