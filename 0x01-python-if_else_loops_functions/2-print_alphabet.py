e1b11640bb95cc46a3e4cdfbac6c2ee56c250a27
Your Name <ahmedabuayoub2@gmail.com>
2024-01-16 11:25:53 -0800

    solved

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
     print('{:c}'.format(i), end='')
-
-
-3-print_alphabet.py
-
-#!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print('{:c}'.format(i), end='')
